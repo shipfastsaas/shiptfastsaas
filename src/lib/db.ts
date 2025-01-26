d0a3dfbdaf53393adde185c198bafeb6dd15cf86
Myohmtechnologies <marketing@myohmtechnologies.com>
2025-01-26 21:31:25 +0100

    fix: replace var with let in global declaration

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 declare global {
-  var mongoose: {
+  let mongoose: {
     conn: typeof mongoose | null
     promise: Promise<typeof mongoose> | null
   } | undefined
