910bfc595c2794af72495a3529ad0baf4602fdea
Myohmtechnologies <marketing@myohmtechnologies.com>
2025-01-27 16:28:12 +0100

    fix: move page.tsx back to root

--- src/app/(marketing)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '@/components/layout/layout'
-
-export default function MarketingLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <Layout>{children}</Layout>
-}
